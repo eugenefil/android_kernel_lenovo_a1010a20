cbc33c2620b3e9ab82769be9e4d85b671f96db9b
Evgeniy Filimonov <fil.eugene@gmail.com>
2023-07-13 14:06:27 +0200

    make.sh: add comment on how to override CONFIG_CROSS_COMPILE

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 mkdir -p out
+# tip: if arm-eabi- cross compiler from defconfig is unwanted, override it
+# with CROSS_COMPILE=<prefix> argument to this script (e.g. use empty
+# CROSS_COMPILE= to not use cross compiler)
 make ARCH=arm O=out -j$(nproc) "$@"
